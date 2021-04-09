ba0ec7bab307d3d490241e7ac0fd986f3b80c026
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-09 11:18:53 +0530

    Working on Posts

--- src/Post.js
+++ src/Post.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Post.css';
 import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({ username, caption, imageUrl }) {
     return (
         <div className="post">
             <div className="post__header">
@@ -11,11 +11,11 @@ function Post() {
                     alt="swapnil"
                     src="/static/images/avatar/1.jpg"
                 />
-                <h3>Username</h3>
+                <h3>{username}</h3>
             </div>
 
-            <img className="post__image" src="https://images.unsplash.com/photo-1615069433711-41e14f250182?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" />
-            <h4 className="post__text"><strong>swapnilhota</strong> learning React</h4>
+            <img className="post__image" src={imageUrl} alt="" />
+            <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
         </div>
     )
 }
