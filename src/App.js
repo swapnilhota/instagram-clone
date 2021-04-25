307ce2494529345ccbc8f24bd438eee73504e6e4
Swapnil Hota <swapnilhota99@gmail.com>
2021-04-25 11:10:28 +0530

    upload image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import { Button, Input } from '@material-ui/core';
+import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
     const top = 50;
@@ -95,6 +96,8 @@ function App() {
 
     return (
         <div className="App">
+
+            <ImageUpload />
 
             <Modal
                 open={open}
---
+++ src/ImageUpload.js
@@ -0,0 +1,31 @@
+import { Button } from '@material-ui/core'
+import React, { useState } from 'react'
+
+function ImageUpload() {
+
+    const [image, setImage] = useState(null);
+    const [progress, setProgress] = useState(0);
+    const [caption, setCaption] = useState('');
+
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleUpload = () => {
+
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="Enter a Caption" onChange={event => setCaption(event.target.value)} value={caption} />
+            <input type="file" onChange={handleChange} />
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>
+        </div>
+    )
+}
+
+export default ImageUpload
